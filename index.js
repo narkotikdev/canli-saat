6684f59f3911661c40ba160376fedbcd664bded3
Narkotik <78884862+narkotikdev@users.noreply.github.com>
2025-08-29 02:53:17 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Canlı Saat</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="clock">
+  <div class="time" id="time">00:00:00</div>
+  <div class="label">Saat : Dakika : Saniye</div>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
